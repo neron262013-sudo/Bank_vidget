ee9cdd7fc58836d17622bf3f3f984be57a9e0429
Роман Шепило <my_mail@my.com>
2025-12-27 03:09:21 +0300

    sort_by_date func

--- src/processing.py
+++ src/processing.py
@@ -10,3 +10,15 @@ def filter_by_state(list_of_data_dicts: list[dict], state: str = "EXECUTED") -> 
             new_list_of_data_dicts.append(i)
 
     return new_list_of_data_dicts
+
+def sort_by_date(list_of_data_dicts: list[dict], sort_order: str = "убывание") -> list[dict]:
+    """Сортирует словари в списке по дате. Можно указать порядок сортировки. Убывание по умолчанию"""
+
+    # Проверяет что введено в sort_order. Если ничего или "убывание", то сортировка по убыванию.
+    # Иначе - по возрастанию.
+    order = False
+    if sort_order == "убывание":
+        order = True
+
+    # Возвращаем отсортированный по дате список словарей
+    return sorted(list_of_data_dicts, key=lambda x: x["date"], reverse=order)
