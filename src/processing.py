965a6afaac6eb12d0581cf6a53fa0e91b88aad3b
Роман Шепило <my_mail@my.com>
2025-12-27 13:34:12 +0300

    fixes without README

--- src/processing.py
+++ src/processing.py
@@ -4,22 +4,16 @@ def filter_by_state(list_of_data_dicts: list[dict], state: str = "EXECUTED") -> 
     # Проходим по словарям в списке. Проверяем, есть ли ключ "state" через .get.
     # Заносим все словари с указанным "state" в новый словарь и возвращаем.
     new_list_of_data_dicts = []
-    for i in list_of_data_dicts:
-        data_state = i.get("state")
+    for data_dict in list_of_data_dicts:
+        data_state = data_dict.get("state")
         if data_state == state:
-            new_list_of_data_dicts.append(i)
+            new_list_of_data_dicts.append(data_dict)
 
     return new_list_of_data_dicts
 
 
-def sort_by_date(list_of_data_dicts: list[dict], sort_order: str = "True") -> list[dict]:
+def sort_by_date(list_of_data_dicts: list[dict], sort_order: bool = True) -> list[dict]:
     """Сортирует словари в списке по дате. Можно указать порядок сортировки. Убывание по умолчанию"""
 
-    # Проверяет что введено в sort_order. Если ничего или "True", то сортировка по убыванию.
-    # Иначе - по возрастанию.
-    order = False
-    if sort_order == "True":
-        order = True
-
     # Возвращаем отсортированный по дате список словарей
-    return sorted(list_of_data_dicts, key=lambda x: x["date"], reverse=order)
+    return sorted(list_of_data_dicts, key=lambda x: x["date"], reverse=sort_order)
