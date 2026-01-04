aac70216b655f7cd0286d7a1780288e2c819e66c
Роман Шепило <my_mail@my.com>
2026-01-04 20:08:25 +0300

    test_processing sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -22,3 +22,25 @@ def sort_by_date(list_of_data_dicts: list[dict], sort_order: bool = True) -> lis
 
     # Возвращаем отсортированный по дате список словарей
     return sorted(list_of_data_dicts, key=lambda x: x["date"], reverse=sort_order)
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 615064591, 'state': 'SOME_TEXT', 'date': '2018-05-14T08:21:33.419441'},
+            {'id': 615064591, 'state': 'ANOTHER_TEXT', 'date': '2018-05-14T08:21:33.419441'}]
+                   , False))
+
+#[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+# {'id': 615064591, 'state': 'SOME_TEXT', 'date': '2018-05-14T08:21:33.419441'},
+# {'id': 615064591, 'state': 'ANOTHER_TEXT', 'date': '2018-05-14T08:21:33.419441'}]
+
+# [{'id': 615064591, 'state': 'SOME_TEXT', 'date': '2018-05-14T08:21:33.419441'},
+# {'id': 615064591, 'state': 'ANOTHER_TEXT', 'date': '2018-05-14T08:21:33.419441'},
+# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+# {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
