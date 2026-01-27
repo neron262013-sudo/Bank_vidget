756efeda638711a87f194b9086932d24f414fa76
Роман <nero.n262013@gmail.com>
2026-01-27 12:47:51 +0300

    fixes and readme

--- src/decorators.py
+++ src/decorators.py
@@ -1,34 +1,34 @@
-from fileinput import filename
 from functools import wraps
 
 
 def log(filename=None):
+    """Декоратор записи логов. Если передать аргумент, то результат работы записывается в файл. Иначе - в консоль"""
+
     def wrapper(func):
         @wraps(func)
         def inner(*args, **kwargs):
             try:
-                result = func(*args, **kwargs)
+                _ = func(*args, **kwargs)
+                # Обработка с аргументом в файл
                 if filename:
                     with open(f"{filename}", "a", encoding="utf-8") as file:
                         file.write(f"{func.__name__} ok\n")
                     return f"{func.__name__} ok\n"
+                # Обработка без аргумента в консоль
                 else:
                     print(f"{func.__name__} ok")
                 return f"{func.__name__} ok"
             except Exception as e:
+                # Обработка ошибки с аргументом в файл
                 if filename:
                     with open(f"{filename}", "a", encoding="utf-8") as file:
                         file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
                         return f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n"
+                # Обработка с ошибкой без аргумента в консоль
                 else:
                     print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
                     return f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
+
         return inner
+
     return wrapper
-
-
-@log()
-def my_function(x, y):
-    return x + y
-
-my_function("1", 2)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,6 @@
 import pytest
-from src.decorators import log, my_function
+
+from src.decorators import log
 
 
 @pytest.fixture
@@ -7,7 +8,9 @@ def function_example():
     @log()
     def my_function(x, y):
         return x + y
+
     return my_function
+
 
 @pytest.fixture
 def function_example_with_parameter(tmp_path):
@@ -19,61 +22,70 @@ def function_example_with_parameter(tmp_path):
 
     return my_function, log_file
 
-@pytest.mark.parametrize("first_num, second_num, expected",
-                         [
-                             (1, 2, "my_function ok"),
-                             (-1, 2, "my_function ok"),
-                             (1, -2, "my_function ok")
-                         ]
-                         )
+
+@pytest.mark.parametrize(
+    "first_num, second_num, expected", [(1, 2, "my_function ok"), (-1, 2, "my_function ok"), (1, -2, "my_function ok")]
+)
 def test_log(function_example, first_num, second_num, expected):
     assert function_example(first_num, second_num) == expected
 
 
-@pytest.mark.parametrize("first_num, second_num, expected",
-                         [
-                             ("", 2, "my_function error: "
-                                     "can only concatenate str (not \"int\") to str. "
-                                     "Inputs: ('', 2), {}"),
-                             (1, "2", "my_function error: "
-                                      "unsupported operand type(s) for +: 'int' and 'str'. "
-                                      "Inputs: (1, '2'), {}"),
-                             (None, 2, "my_function error: "
-                                       "unsupported operand type(s) for +: 'NoneType' and 'int'. "
-                                       "Inputs: (None, 2), {}"),
-                             (1, None, "my_function error: "
-                                       "unsupported operand type(s) for +: 'int' and 'NoneType'. "
-                                       "Inputs: (1, None), {}"),
-                             ([1, 2], None, "my_function error: "
-                                            "can only concatenate list (not \"NoneType\") to list. "
-                                            "Inputs: ([1, 2], None), {}"),
-                             ([1, 2], 3, "my_function error: "
-                                         "can only concatenate list (not \"int\") to list. "
-                                         "Inputs: ([1, 2], 3), {}"),
-                             ({"1":"2"}, None, "my_function error: "
-                                               "unsupported operand type(s) for +: 'dict' and 'NoneType'. "
-                                               "Inputs: ({'1': '2'}, None), {}")
-                         ]
-                         )
+@pytest.mark.parametrize(
+    "first_num, second_num, expected",
+    [
+        ("", 2, "my_function error: " 'can only concatenate str (not "int") to str. ' "Inputs: ('', 2), {}"),
+        (1, "2", "my_function error: " "unsupported operand type(s) for +: 'int' and 'str'. " "Inputs: (1, '2'), {}"),
+        (
+            None,
+            2,
+            "my_function error: " "unsupported operand type(s) for +: 'NoneType' and 'int'. " "Inputs: (None, 2), {}",
+        ),
+        (
+            1,
+            None,
+            "my_function error: " "unsupported operand type(s) for +: 'int' and 'NoneType'. " "Inputs: (1, None), {}",
+        ),
+        (
+            [1, 2],
+            None,
+            "my_function error: " 'can only concatenate list (not "NoneType") to list. ' "Inputs: ([1, 2], None), {}",
+        ),
+        ([1, 2], 3, "my_function error: " 'can only concatenate list (not "int") to list. ' "Inputs: ([1, 2], 3), {}"),
+        (
+            {"1": "2"},
+            None,
+            "my_function error: "
+            "unsupported operand type(s) for +: 'dict' and 'NoneType'. "
+            "Inputs: ({'1': '2'}, None), {}",
+        ),
+    ],
+)
 def test_log_exceptions(function_example, first_num, second_num, expected):
     assert function_example(first_num, second_num) == expected
 
 
-@pytest.mark.parametrize("first_num, second_num, expected",
-                         [
-                             (1, 2, "my_function ok\n"),
-                             (-1, -2, "my_function ok\n"),
-                             ("", 2, "my_function error: "
-                                     "can only concatenate str (not \"int\") to str. "
-                                     "Inputs: ('', 2), {}\n"),
-                             ([1, 2], None, "my_function error: "
-                                            "can only concatenate list (not \"NoneType\") to list. "
-                                            "Inputs: ([1, 2], None), {}\n"),
-                             (None, 2, "my_function error: "
-                                       "unsupported operand type(s) for +: 'NoneType' and 'int'. "
-                                       "Inputs: (None, 2), {}\n")
-                         ]
-                         )
+@pytest.mark.parametrize(
+    "first_num, second_num, expected",
+    [
+        (1, 2, "my_function ok\n"),
+        (-1, -2, "my_function ok\n"),
+        ("", 2, "my_function error: " 'can only concatenate str (not "int") to str. ' "Inputs: ('', 2), {}\n"),
+        (
+            [1, 2],
+            None,
+            "my_function error: "
+            'can only concatenate list (not "NoneType") to list. '
+            "Inputs: ([1, 2], None), {}\n",
+        ),
+        (
+            None,
+            2,
+            "my_function error: "
+            "unsupported operand type(s) for +: 'NoneType' and 'int'. "
+            "Inputs: (None, 2), {}\n",
+        ),
+    ],
+)
 def test_log_capsys(function_example, first_num, second_num, expected, capsys):
     function_example(first_num, second_num)
     captured = capsys.readouterr()
@@ -85,58 +97,39 @@ def test_log_creates_file(function_example_with_parameter):
     my_function(1, 2)
     assert log_file.exists()
 
-@pytest.mark.parametrize("first_num, second_num, expected",
-                         [
-                             ("", 2, "my_function error: "
-                                     "can only concatenate str (not \"int\") to str. "
-                                     "Inputs: ('', 2), {}"),
-                             (1, "2", "my_function error: "
-                                      "unsupported operand type(s) for +: 'int' and 'str'. "
-                                      "Inputs: (1, '2'), {}"),
-                             (None, 2, "my_function error: "
-                                       "unsupported operand type(s) for +: 'NoneType' and 'int'. "
-                                       "Inputs: (None, 2), {}"),
-                             (1, None, "my_function error: "
-                                       "unsupported operand type(s) for +: 'int' and 'NoneType'. "
-                                       "Inputs: (1, None), {}"),
-                             ([1, 2], None, "my_function error: "
-                                            "can only concatenate list (not \"NoneType\") to list. "
-                                            "Inputs: ([1, 2], None), {}"),
-                             ([1, 2], 3, "my_function error: "
-                                         "can only concatenate list (not \"int\") to list. "
-                                         "Inputs: ([1, 2], 3), {}"),
-                             ({"1":"2"}, None, "my_function error: "
-                                               "unsupported operand type(s) for +: 'dict' and 'NoneType'. "
-                                               "Inputs: ({'1': '2'}, None), {}")
-                         ]
-                         )
+
+@pytest.mark.parametrize(
+    "first_num, second_num, expected",
+    [
+        ("", 2, "my_function error: " 'can only concatenate str (not "int") to str. ' "Inputs: ('', 2), {}"),
+        (1, "2", "my_function error: " "unsupported operand type(s) for +: 'int' and 'str'. " "Inputs: (1, '2'), {}"),
+        (
+            None,
+            2,
+            "my_function error: " "unsupported operand type(s) for +: 'NoneType' and 'int'. " "Inputs: (None, 2), {}",
+        ),
+        (
+            1,
+            None,
+            "my_function error: " "unsupported operand type(s) for +: 'int' and 'NoneType'. " "Inputs: (1, None), {}",
+        ),
+        (
+            [1, 2],
+            None,
+            "my_function error: " 'can only concatenate list (not "NoneType") to list. ' "Inputs: ([1, 2], None), {}",
+        ),
+        ([1, 2], 3, "my_function error: " 'can only concatenate list (not "int") to list. ' "Inputs: ([1, 2], 3), {}"),
+        (
+            {"1": "2"},
+            None,
+            "my_function error: "
+            "unsupported operand type(s) for +: 'dict' and 'NoneType'. "
+            "Inputs: ({'1': '2'}, None), {}",
+        ),
+    ],
+)
 def test_log_check_content(function_example_with_parameter, first_num, second_num, expected):
     my_function, log_file = function_example_with_parameter
     my_function(first_num, second_num)
     content = log_file.read_text(encoding="utf-8")
     assert expected in content
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
